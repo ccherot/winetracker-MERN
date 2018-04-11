543fd02c226beba122d9de264f3de0c9586f41ee
Colin Cherot <ccherot@sonic.net>
2018-04-11 16:54:27 -0700

    adding some changes to the server.js file to get it to use the weback middlewares for dev only.  I am still not sure how to use server-side routing along with React's client side routing.

--- server.js
+++ server.js
@@ -1,6 +1,42 @@
 /* eslint no-console:0 */
+require("babel-register");
+
+// first require all of the Reacat / Webpack stuff
+const React = require("react");
+const ReactDOMServer = require("react-dom/server");
+const ReactRouter = require("react-router-dom");
+const _ = require("lodash");
+const fs = require("fs");
+const webpackDevMiddleware = require("webpack-dev-middleware");
+const webpackHotMiddleware = require("webpack-hot-middleware");
+const compression = require("compression");
+const webpack = require("webpack");
+const config = require("./webpack.config");
+const App = require("./src/js/App").default;
+
+const StaticRouter = ReactRouter.StaticRouter;
+const port = 8080;
+const baseTemplate = fs.readFileSync("./index.html");
+const template = _.temmplate(baseTemplate);
+
 // require express
 const express = require("express");
+
+// create an express app
+const server = express();
+
+// tell the server to use compression
+server.use(compression());
+// if we are in dev mode then we need to tell the
+// server to use webpack and the dev middleware
+// and hot module reloading middleware and all that
+// fun stuff
+if (process.env.NODE_ENV === "development") {
+  const compiler = webpack(config);
+  server.use(webpackDevMiddleware(compiler, { publicPath: config.output.publicPath }));
+  server.use(webpackHotMiddleware(compiler));
+}
+server.use("/public", express.static("./public"));
 
 // require path
 const path = require("path");
@@ -8,17 +44,14 @@ const path = require("path");
 // require body-parser
 const bodyParser = require("body-parser");
 
-// create an express app
-const app = express();
-
 // tell express to use body parser and to use extended
 // and json so that we can parse the json in the req.body
-// app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json());
+// server.use(bodyParser.urlencoded({extended: true}))
+server.use(bodyParser.json());
 
 // tell express where the static files are.  This is the
-// dist folder for our Angular front end code
-app.use(express.static(path.join(__dirname, "./public")));
+// public folder for our React front end code
+server.use(express.static(path.join(__dirname, "./public")));
 
 // require mongoose for our MongoDB connectivity
 require("./server/config/mongoose.js");
@@ -28,9 +61,9 @@ const routeSetter = require("./server/config/routes.js");
 
 // invoke the function in the routes_setter and pass
 // a reference to our express server
-routeSetter(app);
+routeSetter(server);
 
 // finally start the server
-app.listen(8000, () => {
+server.listen(8000, () => {
   console.log("winetracker listening on port 8000!!!");
 });
--- server/config/routes.js
+++ server/config/routes.js
@@ -1,142 +1,134 @@
-//we are going to need the following routes
+// we are going to need the following routes
 
-//need path because of the last route that is handling the 
-//case where the use types in a URL
-var path = require('path')
+// need path because of the last route that is handling the
+// case where the use types in a URL
+// const path = require("path");
 
-//need reference to controllers for wines and users
-var wines = require('../controllers/wines.js')
-var users = require("../controllers/users.js")
-var cellars = require("../controllers/cellars.js")
-var cellarItems = require("../controllers/cellarItems.js")
+// need reference to controllers for wines and users
+const wines = require("../controllers/wines.js");
+const users = require("../controllers/users.js");
+const cellars = require("../controllers/cellars.js");
+const cellarItems = require("../controllers/cellarItems.js");
 
-// this adds route listeners to users and wines 
-module.exports = function(app){
+//  this adds route listeners to users and wines
+module.exports = function(app) {
+  // these are the user related routes
 
-    //these are the user related routes
+  // TODO: do we want future functionality for
+  // searching other user's cellars (if they are made public?)
 
-    //TODO: do we want future functionality for 
-    //searching other user's cellars (if they are made public?)
+  // this will get all the users
+  app.get("/users", (req, res) => {
+    users.getUsers(req, res);
+  });
 
-    //this will get all the users
-    app.get('/users', function(req, res) {
-        users.getUsers(req, res);
-    });
+  // this should create a new user
+  app.post("/register", (req, res) => {
+    users.register(req, res);
+  });
 
-    //this should create a new user
-    app.post('/register', function(req, res) {
-        users.register(req, res);
-    });
+  // this should attempt to login an existing user
+  app.post("/login", (req, res) => {
+    users.login(req, res);
+  });
 
-    //this should attempt to login an existing user
-    app.post('/login', function(req, res) {
-        users.login(req, res);
-    });
+  // this will delete a user
+  app.delete("/users/:id", (req, res) => {
+    users.deleteUser(req, res);
+  });
 
+  // this will update a user's profile info
+  app.patch("/users", (req, res) => {
+    users.updateUser(req, res);
+  });
 
-    //this will delete a user
-    app.delete('/users/:id', function(req, res) {
-        users.deleteUser(req, res);
-    });
+  // this will allow a user to change their password
+  app.post("/users/changepassword", (req, res) => {
+    users.changePassword(req, res);
+  });
 
-    //this will update a user's profile info
-    app.patch('/users', function(req, res) {
-        users.updateUser(req, res);
-    });
+  //
+  // Wine Routes
+  //
 
-    //this will allow a user to change their password
-    app.post('/users/changepassword', function (req, res) {
-        users.changePassword(req, res);
-    })
+  // this will get all the wines in the db. This can be useful for initially populating a cellar
+  // this should probably paginate somehow or maybe the process can be broken up
+  // TODO: how to get n number of records and then get the next n records
+  // this might not be a good route to use...maybe force users to search by region before adding?
+  app.get("/wines", (req, res) => {
+    wines.getAllWines(req, res);
+  });
 
+  // This route searches ALL wines using a given query
+  app.get("/winesearch", (req, res) => {
+    wines.searchWines(req, res);
+  });
 
-    //
-    //Wine Routes
-    //
+  // this will post new wines
+  app.post("/wines", (req, res) => {
+    wines.addWine(req, res);
+  });
 
-    //this will get all the wines in the db. This can be useful for initially populating a cellar 
-    //this should probably paginate somehow or maybe the process can be broken up 
-    //TODO: how to get n number of records and then get the next n records 
-    //this might not be a good route to use...maybe force users to search by region before adding?
-    app.get('/wines', function(req, res) {
-        wines.getAllWines(req, res);
-    });
+  // this will delete a wine.  this should
+  // only be done by an administrator
+  app.delete("/wines/:id", (req, res) => {
+    wines.deleteWine(req, res);
+  });
 
-    //This route searches ALL wines using a given query
-    app.get('/winesearch', function(req, res) {
-        wines.searchWines(req, res);
-    });
+  // this will update a wine.  Should this be an admin only route?
+  app.patch("/wines", (req, res) => {
+    wines.updateWine(req, res);
+  });
 
-    //this will post new wines
-    app.post('/wines', function(req, res) {
-        wines.addWine(req, res);
-    });
+  //
+  // CELLAR ROUTES
+  //
 
-    //this will delete a wine.  this should 
-    //only be done by an administrator
-    app.delete('/wines/:id', function(req, res) {
-        wines.deleteWine(req, res);
-    });
+  // these routes are for user cellars so they are more
+  // related to collections of wines than individual wines.
+  // This app could be useful for professional wine cellar managers
 
-    //this will update a wine.  Should this be an admin only route?  
-    app.patch('/wines', function(req, res) {
-        wines.updateWine(req, res);
-    });
+  // this should create a new cellar attached to a user
+  app.post("/cellar", (req, res) => {
+    cellars.addCellar(req, res);
+  });
 
-    //
-    //CELLAR ROUTES
-    //
+  // this gets all the wines in a specific user's cellar
+  app.get("/cellar/:id", (req, res) => {
+    cellars.getCellar(req, res);
+  });
 
-    //these routes are for user cellars so they are more 
-    //related to collections of wines than individual wines.
-    //This app could be useful for professional wine cellar managers
+  // this route deletes a cellar from a user's profile.
+  // probably not going to be used much...I hope
+  app.delete("/cellar/:id", (req, res) => {
+    cellars.deleteCellar(req, res);
+  });
 
-    //this should create a new cellar attached to a user
-    app.post('/cellar', function(req, res) {
-        cellars.addCellar(req, res);
-    });
+  // this route allows a user to update a cellar name
+  app.patch("/cellar", (req, res) => {
+    cellars.updateCellar(req, res);
+  });
 
-        
-    //this gets all the wines in a specific user's cellar
-    app.get('/cellar/:id', function (req, res){
-        cellars.getCellar(req, res) 
-    })
+  //
+  // CellarItem Routes
+  //
 
-    //this route deletes a cellar from a user's profile.
-    //probably not going to be used much...I hope
-    app.delete('/cellar/:id', function (req, res){
-        cellars.deleteCellar(req, res)
-    })
+  app.patch("/cellaritem", (req, res) => {
+    cellarItems.updateCellarItem(req, res);
+  });
 
-    //this route allows a user to update a cellar name
-    app.patch('/cellar', function (req, res){
-        cellars.updateCellar(req, res)
-    })
+  // this route adds a new cellar item to the cellar
+  app.post("/cellaritem", (req, res) => {
+    cellarItems.addToCellar(req, res);
+  });
 
-    //
-    //CellarItem Routes
-    //
-
-    app.patch('/cellaritem', function (req, res) {
-        cellarItems.updateCellarItem(req, res)
-    })
-
-    //this route adds a new cellar item to the cellar
-    app.post('/cellaritem', function (req, res){
-        cellarItems.addToCellar(req, res)
-    })
-
-
-    //this route deletes a CellarItem.  This will likely not get used
-    //unless there is something very wring wit the CellarItem or maybe if
-    //it is a duplicate.  Most of the time a CellarItems quantity will simply
-    //be adjusted or the info will be edited in some way.  CellarItems with a 
-    //quantity of 0 will be moved to a different list of 'consumed' wines 
-    //perhaps.  
-    app.delete('/cellaritem/:id', function (req, res){
-        cellarItems.deleteFromCellar(req, res)
-    })
-
-
-
-}
+  // this route deletes a CellarItem.  This will likely not get used
+  // unless there is something very wring wit the CellarItem or maybe if
+  // it is a duplicate.  Most of the time a CellarItems quantity will simply
+  // be adjusted or the info will be edited in some way.  CellarItems with a
+  // quantity of 0 will be moved to a different list of 'consumed' wines
+  // perhaps.
+  app.delete("/cellaritem/:id", (req, res) => {
+    cellarItems.deleteFromCellar(req, res);
+  });
+};
