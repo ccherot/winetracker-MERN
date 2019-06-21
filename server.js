ec96211265f00947332d04ae5ee48d1043de13ff
Colin Cherot <ccherot@sonic.net>
2019-06-21 15:33:46 -0700

    deleting a bunch of stuff.  I deleted the node_modules directory and then removed all packages from package.json and then re-added them with yarn

--- server.js
+++ server.js
@@ -1,25 +1,39 @@
 /* eslint no-console:0 */
-require("babel-register");
+
+// for now I am going to remove all of the React and Webpack code from
+// the server.  I might not add webpack back in if I can get away with using
+// Parcel instead although it makes a lot of sense to figure out how to get
+// all of this working with webpack.  Itm might make sense to have two different
+// server.js files at some point but I am not sure yet.
+
+// I will prefix comments with OLD to denote stuff that was using
+// webpack for the server
+
+// TEMP
+// require("babel-register");
 
 // first require all of the Reacat / Webpack stuff
-const React = require("react");
-const ReactDOMServer = require("react-dom/server");
-const ReactRouter = require("react-router-dom");
-// const _ = require("lodash");
-const fs = require("fs");
-const webpackDevMiddleware = require("webpack-dev-middleware");
-const webpackHotMiddleware = require("webpack-hot-middleware");
-const compression = require("compression");
-const webpack = require("webpack");
-const config = require("./webpack.config");
-const App = require("./src/js/App").default;
+// TEMP const React = require("react");
+// TEMP const ReactDOMServer = require("react-dom/server");
+// TEMP const ReactRouter = require("react-router-dom");
 
-const staticRouter = ReactRouter.StaticRouter; // { StaticRouter }
+// file system module that we need for various things
+const fs = require("fs");
+// START TEMP
+// const webpackDevMiddleware = require("webpack-dev-middleware");
+// const webpackHotMiddleware = require("webpack-hot-middleware");
+// const compression = require("compression");
+// const webpack = require("webpack");
+// const config = require("./webpack.config");
+// const App = require("./src/js/App").default;
+
+// const staticRouter = ReactRouter.StaticRouter; // { StaticRouter }
+// we DO need the port regardless
 const port = 8080;
-const baseTemplate = fs.readFileSync("./index.html");
-const templateFunc = require("lodash.template");
-// _.temmplate(baseTemplate);
-const template = templateFunc(baseTemplate);
+// const baseTemplate = fs.readFileSync("./index.html");
+// const templateFunc = require("lodash.template");
+// const template = templateFunc(baseTemplate);
+// END TEMP
 
 // require express
 const express = require("express");
@@ -28,32 +42,25 @@ const express = require("express");
 const server = express();
 
 // tell the server to use compression
-server.use(compression());
+// TEMP I don't think we need the compression if we
+// TEMP are not using webpack
+// TEMP server.use(compression());
+
 // if we are in dev mode then we need to tell the
 // server to use webpack and the dev middleware
 // and hot module reloading middleware and all that
 // fun stuff
-if (process.env.NODE_ENV === "development") {
-  const compiler = webpack(config);
-  server.use(webpackDevMiddleware(compiler, { publicPath: config.output.publicPath }));
-  server.use(webpackHotMiddleware(compiler));
-}
-server.use("/public", express.static("./public"));
+// START TEMP
+// if (process.env.NODE_ENV === "development") {
+//   const compiler = webpack(config);
+//   server.use(webpackDevMiddleware(compiler, { publicPath: config.output.publicPath }));
+//   server.use(webpackHotMiddleware(compiler));
+// }
+// server.use("/public", express.static("./public"));
 
 // here is where we hook up the React parts for the client side
 // routing
-server.use((req, res) => {
-  const context = {};
-  const body = ReactDOMServer.renderToString(
-    React.createElement(staticRouter, { location: req.url, context }, React.createElement(App))
-  );
-  if (context.url) {
-    res.redirect(301, context.url);
-  }
-
-  res.write(template({ body }));
-  res.end();
-});
+// // END TEMP
 
 // require body-parser
 const bodyParser = require("body-parser");
